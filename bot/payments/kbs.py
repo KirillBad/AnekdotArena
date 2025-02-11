07c082c3b9bdb4c96f91fc64852efb5892ee19e4
BluePencilBord <amin54321@yandex.ru>
2025-02-11 11:38:50 +0300

    added send stars invoice

--- bot/payments/kbs.py
+++ bot/payments/kbs.py
@@ -1,8 +1,18 @@
 from aiogram.utils.keyboard import InlineKeyboardBuilder  
+from aiogram.filters.callback_data import CallbackData
+from typing import Optional
+
+
+class GiveStarsCallbackFactory(CallbackData, prefix="give"):
+    action: str
+    value: Optional[int] = None
   
-  
+
 def give_stars_kb():  
     builder = InlineKeyboardBuilder()  
-    builder.button(text=f"Оплатить 10 ⭐️", pay=True, callback_data="give_stars_10")  
-  
+    builder.button(text=f"30 ⭐️", callback_data=GiveStarsCallbackFactory(action="stars", value=30))
+    builder.button(text=f"50 ⭐️", callback_data=GiveStarsCallbackFactory(action="stars", value=50)) 
+    builder.button(text=f"100 ⭐️", callback_data=GiveStarsCallbackFactory(action="stars", value=100))   
+    builder.button(text="Назад", callback_data="back")
+    builder.adjust(1, 1, 1)
     return builder.as_markup()
--- bot/payments/router.py
+++ bot/payments/router.py
@@ -1,30 +1,35 @@
 from aiogram import Router, F
-from aiogram.types import CallbackQuery
+from aiogram.types import CallbackQuery, LabeledPrice
 from aiogram.fsm.context import FSMContext
 from anecdotes.states import RateStates
 from payments.kbs import give_stars_kb
 from anecdotes.kbs import rate_anecdote_kb
+from payments.kbs import GiveStarsCallbackFactory
 
 payments_router = Router()
 
 
-@payments_router.callback_query(F.data == "give_stars")
+@payments_router.callback_query(F.data == "select_stars")
 async def start_give_stars(callback: CallbackQuery, state: FSMContext):
+    await state.set_state(RateStates.selecting_stars_amount)
     await callback.message.edit_reply_markup(
         reply_markup=give_stars_kb(),
     )
-    await state.set_state(RateStates.selecting_stars_amount)
 
-
-# @payments_router.callback_query(
-#     StarsCallbackFactory.filter(F.action == "give"), RateStates.selecting_stars_amount
-# )
-# async def process_give_stars(
-#     callback: CallbackQuery, callback_data: StarsCallbackFactory, state: FSMContext
-# ):
-#     await callback.message.edit_reply_markup(
-#         reply_markup=give_stars_kb(),
-#     )
+@payments_router.callback_query(
+    GiveStarsCallbackFactory.filter(F.action == "stars"), RateStates.selecting_stars_amount
+)
+async def process_give_stars(
+    callback: CallbackQuery, callback_data: GiveStarsCallbackFactory, state: FSMContext
+):
+    await callback.message.reply_invoice(
+        title="Задонатить автору анекдота",
+        description="Автор получит подарок на сумму эквивалентную количеству звезд (подарок можно сконвертировать в ⭐️ при получении) за вычетом комиссии 10%",
+        payload="test",
+        provider_token="",
+        prices=[LabeledPrice(label="XTR", amount=callback_data.value)],
+        currency="XTR",
+    )
 
 @payments_router.callback_query(F.data == "back", RateStates.selecting_stars_amount)
 async def handle_back(callback: CallbackQuery, state: FSMContext):
